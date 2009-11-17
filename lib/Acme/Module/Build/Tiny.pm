ae40565651adc89d3b5522011353084640ffce6f
David Golden <dagolden@cpan.org>
2009-11-17 11:35:38 -0500

    simple build & test working

--- lib/Acme/Module/Build/Tiny.pm
+++ lib/Acme/Module/Build/Tiny.pm
@@ -1,9 +1,15 @@
 package Acme::Module::Build::Tiny;
 use strict;
 use warnings;
-use File::Copy 0 qw(copy);
-use File::Path 0 qw(mkpath rmtree);
+use ExtUtils::Install 0 ();
+use File::Copy 0 ();
+use File::Find 0 ();
+use File::Path 0 ();
+use File::Spec 0 ();
+use Test::Harness 0 ();
 use Tie::File 0 ();
+
+my %re = ( lib => qr{\.(?:pm|pod)$}, t => qr{\.t} );
 
 run(@ARGV) unless caller; # modulino :-)
 
@@ -12,20 +18,21 @@ sub run {
   __PACKAGE__->$action() or exit 1;
 }
 
-sub import { shift->configure }
-
-sub configure {
-  copy $INC{_mod2pm(shift)}, 'Build' or die $!;
+sub import {
+  File::Copy::copy $INC{_mod2pm(shift)}, 'Build' or die $!;
   chmod 0755, 'Build';
   tie my @file, 'Tie::File', 'Build';
   unshift @file, "#!$^X";
 }
 
 sub build {
+  ExtUtils::Install::pm_to_blib({ map {$_=>"blib/$_"} _files('lib')}, 'blib/lib/auto') || 1;
 }
 
 sub test {
-
+  build();
+  local @INC = (File::Spec->rel2abs('blib/lib'), @INC);
+  Test::Harness::runtests(_files('t'));
 }
 
 sub install {
@@ -36,11 +43,15 @@ sub dist {
 
 }
 
-sub clean {
-  rmtree('Build');
-}
+sub clean { File::Path::rmtree($_) for qw/Build blib/; 1; }
 
 sub _mod2pm { (my $mod = shift) =~ s{::}{/}g; return "$mod.pm" }
+sub _pm2mod { (my $pm  = shift) =~ s{/}{::}g; return substr $pm, 0, -3 }
+
+sub _files { my ($dir,@f) = shift;
+  File::Find::find( sub { -f && /$re{$dir}/ && push @f, $File::Find::name},$dir);
+  return @f;
+}
 
 1;
 
