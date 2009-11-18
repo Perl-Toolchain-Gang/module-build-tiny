beafeefbfff51b3b94594eef6f6a4dcaed9eddfa
David Golden <dagolden@cpan.org>
2009-11-18 00:08:22 -0500

    refactor

--- lib/Acme/Module/Build/Tiny.pm
+++ lib/Acme/Module/Build/Tiny.pm
@@ -12,7 +12,11 @@ use Test::Harness 0 ();
 use Tie::File 0 ();
 our $VERSION = 1;
 
-my %re = ( lib => qr{\.(?:pm|pod)$}, t => qr{\.t} );
+my %re = (
+  lib => qr{\.(?:pm|pod)$},
+  t => qr{\.t},
+  prereq => qr{^\s*use\s+(\S+)\s+(v?[0-9._]+)}
+);
 
 run(@ARGV) unless caller; # modulino :-)
 
@@ -25,10 +29,8 @@ sub import {
   my @f = _files('lib');
   print "Creating new 'Build' script for '" . _mod2dist(_path2mod($f[0])) .
         "' version '" . MM->parse_version($f[0]) . "'\n";
-  File::Copy::copy $INC{_mod2pm(shift)}, 'Build' or die $!;
+  _spew('Build' => "#!$^X\n", _slurp( $INC{_mod2pm(shift)} ) );
   chmod 0755, 'Build';
-  tie my @file, 'Tie::File', 'Build';
-  unshift @file, "#!$^X";
   File::Path::mkpath '_build';
   open my $fh, '>', '_build/prereqs';
   print {$fh} _data_dump(_find_prereqs());
@@ -49,32 +51,30 @@ sub install {
 }
 
 sub dist {
-
+  require ExtUtils::Manifest;
+  _spew('MANIFEST.SKIP', "#!include_default\n") unless -f 'MANIFEST.SKIP';
+  ExtUtils::Manifest::mkmanifest();
+  # XXX ExtUtils::Manifest->VERSION(1.57);
 }
 
 sub clean { File::Path::rmtree($_) for qw/Build blib _build/; 1; }
 
+sub _slurp { do { local (@ARGV,$/)=$_[0]; <> } }
+sub _spew { open my $fh, '>', shift; print {$fh} @_ }
 sub _mod2pm   { (my $mod = shift) =~ s{::}{/}g; return "$mod.pm" }
 sub _path2mod { (my $pm  = shift) =~ s{/}{::}g; return substr $pm, 5, -3 }
 sub _mod2dist { (my $mod = shift) =~ s{::}{-}g; return $mod; }
-
 sub _files { my ($dir,@f) = shift;
   File::Find::find( sub { -f && /$re{$dir}/ && push @f, $File::Find::name},$dir);
   return sort { length $a <=> length $b } @f;
 }
-
 sub _data_dump {
   'do{ my ' . Data::Dumper->new([shift],['x'])->Purity(1)->Dump() . '$x; }'
 }
-
-sub _slurp { do { local (@ARGV,$/)=$_[0]; <> } }
-
 sub _find_prereqs {
   my %requires;
   for my $guts ( map { _slurp($_) } _files('lib') ) {
-    while ( $guts =~ m{^\s*use\s+(\S+)\s+(v?[0-9._]+)}msgc ) {
-      $requires{$1}=$2;
-    }
+    while ( $guts =~ m{$re{prereq}}msgc ) { $requires{$1}=$2; }
   }
   return { requires => \%requires };
 }
