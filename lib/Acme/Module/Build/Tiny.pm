0395fca4c37900fc8d892c8983462eeeda019ed8
David Golden <dagolden@cpan.org>
2009-11-17 23:40:45 -0500

    simple prereqs and Build.PL diag output

--- lib/Acme/Module/Build/Tiny.pm
+++ lib/Acme/Module/Build/Tiny.pm
@@ -1,13 +1,16 @@
 package Acme::Module::Build::Tiny;
 use strict;
 use warnings;
+use Data::Dumper 0 ();
 use ExtUtils::Install 0 ();
+use ExtUtils::MakeMaker 0 ();
 use File::Copy 0 ();
 use File::Find 0 ();
 use File::Path 0 ();
 use File::Spec 0 ();
 use Test::Harness 0 ();
 use Tie::File 0 ();
+our $VERSION = 1;
 
 my %re = ( lib => qr{\.(?:pm|pod)$}, t => qr{\.t} );
 
@@ -19,10 +22,16 @@ sub run {
 }
 
 sub import {
+  my @f = _files('lib');
+  print "Creating new 'Build' script for '" . _mod2dist(_path2mod($f[0])) .
+        "' version '" . MM->parse_version($f[0]) . "'\n";
   File::Copy::copy $INC{_mod2pm(shift)}, 'Build' or die $!;
   chmod 0755, 'Build';
   tie my @file, 'Tie::File', 'Build';
   unshift @file, "#!$^X";
+  File::Path::mkpath '_build';
+  open my $fh, '>', '_build/prereqs';
+  print {$fh} _data_dump(_find_prereqs());
 }
 
 sub build {
@@ -43,14 +52,31 @@ sub dist {
 
 }
 
-sub clean { File::Path::rmtree($_) for qw/Build blib/; 1; }
+sub clean { File::Path::rmtree($_) for qw/Build blib _build/; 1; }
 
-sub _mod2pm { (my $mod = shift) =~ s{::}{/}g; return "$mod.pm" }
-sub _pm2mod { (my $pm  = shift) =~ s{/}{::}g; return substr $pm, 0, -3 }
+sub _mod2pm   { (my $mod = shift) =~ s{::}{/}g; return "$mod.pm" }
+sub _path2mod { (my $pm  = shift) =~ s{/}{::}g; return substr $pm, 5, -3 }
+sub _mod2dist { (my $mod = shift) =~ s{::}{-}g; return $mod; }
 
 sub _files { my ($dir,@f) = shift;
   File::Find::find( sub { -f && /$re{$dir}/ && push @f, $File::Find::name},$dir);
-  return @f;
+  return sort { length $a <=> length $b } @f;
+}
+
+sub _data_dump {
+  'do{ my ' . Data::Dumper->new([shift],['x'])->Purity(1)->Dump() . '$x; }'
+}
+
+sub _slurp { do { local (@ARGV,$/)=$_[0]; <> } }
+
+sub _find_prereqs {
+  my %requires;
+  for my $guts ( map { _slurp($_) } _files('lib') ) {
+    while ( $guts =~ m{^\s*use\s+(\S+)\s+(v?[0-9._]+)}msgc ) {
+      $requires{$1}=$2;
+    }
+  }
+  return { requires => \%requires };
 }
 
 1;
