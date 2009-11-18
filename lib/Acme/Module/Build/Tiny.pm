9e95d7ac4246785bf9c1755e94c77d3c66360589
David Golden <dagolden@cpan.org>
2009-11-18 00:51:24 -0500

    simple dist build

--- lib/Acme/Module/Build/Tiny.pm
+++ lib/Acme/Module/Build/Tiny.pm
@@ -10,7 +10,7 @@ use File::Path 0 ();
 use File::Spec 0 ();
 use Test::Harness 0 ();
 use Tie::File 0 ();
-our $VERSION = 1;
+our $VERSION = '0.01';
 
 my %re = (
   lib => qr{\.(?:pm|pod)$},
@@ -50,13 +50,29 @@ sub install {
 
 }
 
-sub dist {
+sub distdir {
   require ExtUtils::Manifest; ExtUtils::Manifest->VERSION(1.57);
-  _spew('MANIFEST.SKIP', "#!include_default\n") unless -f 'MANIFEST.SKIP';
+  File::Path::rmtree(_distdir());
+  _spew('MANIFEST.SKIP', "#!include_default\n^"._distbase()."\n") unless -f 'MANIFEST.SKIP';
   ExtUtils::Manifest::mkmanifest();
+  ExtUtils::Manifest::manicopy( ExtUtils::Manifest::maniread(), _distdir() );
+}
+
+sub dist {
+  require Archive::Tar; Archive::Tar->VERSION(1.09);
+  distdir();
+  my ($distdir,@f) = (_distdir(),_files(_distdir()));
+  no warnings 'once';
+  $Archive::Tar::DO_NOT_USE_PREFIX = (grep { length($_) >= 100 } @f) ? 0 : 1;
+  my $tar = Archive::Tar->new;
+  $tar->add_files(@f);
+  $_->mode($_->mode & ~022) for $tar->get_files;
+  $tar->write("$distdir.tar.gz", 1);
+  File::Path::rmtree($distdir);
 }
 
 sub clean { File::Path::rmtree('blib'); 1 }
+
 sub realclean { clean(); File::Path::rmtree($_) for qw/Build _build/; 1; }
 
 sub _slurp { do { local (@ARGV,$/)=$_[0]; <> } }
@@ -65,9 +81,12 @@ sub _mod2pm   { (my $mod = shift) =~ s{::}{/}g; return "$mod.pm" }
 sub _path2mod { (my $pm  = shift) =~ s{/}{::}g; return substr $pm, 5, -3 }
 sub _mod2dist { (my $mod = shift) =~ s{::}{-}g; return $mod; }
 sub _files { my ($dir,@f) = shift;
-  File::Find::find( sub { -f && /$re{$dir}/ && push @f, $File::Find::name},$dir);
+  my $regex = $re{$dir} || qr/./;
+  File::Find::find( sub { -f && /$regex/ && push @f, $File::Find::name},$dir);
   return sort { length $a <=> length $b } @f;
 }
+sub _distbase { my @f = _files('lib'); return _mod2dist(_path2mod($f[0])) }
+sub _distdir { my @f = _files('lib'); return _distbase . "-" . MM->parse_version($f[0]) }
 sub _data_dump {
   'do{ my ' . Data::Dumper->new([shift],['x'])->Purity(1)->Dump() . '$x; }'
 }
