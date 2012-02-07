0634d0b122c291ba1d606e70b50cfa219a7413b3
Leon Timmermans <fawaka@gmail.com>
2012-02-07 18:30:45 +0100

    Rewrote to use Build::Simple

--- lib/Module/Build/Tiny.pm
+++ lib/Module/Build/Tiny.pm
@@ -4,13 +4,14 @@ use warnings;
 use Exporter 5.57 'import';
 our @EXPORT = qw/Build Build_PL/;
 
+use Build::Simple;
 use CPAN::Meta;
 use ExtUtils::BuildRC 0.003 qw/read_config/;
 use ExtUtils::Config 0.003;
 use ExtUtils::Helpers 0.012 qw/make_executable split_like_shell man1_pagename man3_pagename detildefy/;
 use ExtUtils::Install qw/pm_to_blib install/;
 use ExtUtils::InstallPaths 0.002;
-use File::Basename qw/dirname/;
+use File::Basename qw/basename dirname/;
 use File::Find::Rule qw/find/;
 use File::Path qw/mkpath/;
 use File::Slurp qw/read_file write_file/;
@@ -28,49 +29,62 @@ sub _manify {
 	my ($input_file, $output_file, $section, $opts) = @_;
 	my $dirname = dirname($output_file);
 	mkpath($dirname, $opts->{verbose}) if not -d $dirname;
+	print "pod2man $input_file > $output_file\n";
 	Pod::Man->new(section => $section)->parse_from_file($input_file, $output_file);
-	print "Manifying $output_file\n" if $opts->{verbose} && $opts->{verbose} > 0;
 	return;
 }
 
-my %actions = (
-	build => sub {
-		my %opt = @_;
-		system $^X, $_ and die "$_ returned $?\n" for find(file => name => '*.PL', in => 'lib');
-		my %modules = map { $_ => catfile('blib', $_) } find(file => name => [qw/*.pm *.pod/], in => 'lib');
-		my %scripts = map { $_ => catfile('blib', $_) } find(file => name => '*', in => 'script');
-		pm_to_blib({ %modules, %scripts }, catdir(qw/blib lib auto/));
-		make_executable($_) for values %scripts;
+sub _get_graph {
+	my %opt = @_;
 
+	my $graph = Build::Simple->new;
+
+	$graph->add_phony('build');
+	$graph->add_phony($_, dependents => ['build']) for qw/PL code man/;
+
+	my %PL_files = map { $_ => basename($_, '.PL') } find(file => name => '*.PL', in => 'lib');
+	$graph->add_file($PL_files{$_}, dependencies => ['Build'], dependents => ['PL'], action => sub { my $file = shift; system $^X, $file and die "$file returned $?\n" } ) for keys %PL_files;
+
+	for my $source (find(file => name => [qw/*.pm *.pod/], in => 'lib'), grep { / \. p(m|od) $ /x } values %PL_files) {
+		my $destination = catfile('blib', $source);
+		$graph->add_file($destination, dependencies => [ $source, 'Build' ], dependents => ['code'], action => sub { pm_to_blib({ $source, $destination }, catdir(qw/blib lib auto/)) });
 		if ($opt{install_paths}->is_default_installable('libdoc')) {
-			_manify($_, catfile('blib', 'bindoc', man1_pagename($_)), 1, \%opt) for keys %scripts;
-			_manify($_, catfile('blib', 'libdoc', man3_pagename($_)), 3, \%opt) for keys %modules;
+			my $manpage = catfile('blib', 'libdoc', man3_pagename($source));
+			$graph->add_file($manpage, dependencies => [ $source, 'Build' ], dependents => ['man'], action => sub { _manify($source, $manpage, 3, \%opt) }) ;
 		}
-	},
-	test => sub {
-		my %opt = @_;
-		die "Must run `./Build build` first\n" if not -d 'blib';
-		my $tester = TAP::Harness->new({verbosity => $opt{verbose}, lib => rel2abs(catdir(qw/blib lib/)), color => -t STDOUT});
-		$tester->runtests(sort +find(file => name => '*.t', in => 't'))->has_errors and exit 1;
-	},
-	install => sub {
-		my %opt = @_;
-		die "Must run `./Build build` first\n" if not -d 'blib';
-		install($opt{install_paths}->install_map, @opt{qw/verbose dry_run uninst/});
-	},
-);
+	}
+
+	for my $source (find(file => name => '*', in => 'script')) {
+		my $destination = catfile('blib', $source);
+		$graph->add_file($destination, dependencies => [ $source, 'Build' ], dependents => ['code'], action => sub {
+			pm_to_blib( { $source => $destination}, catdir(qw/blib lib auto/));
+			make_executable($destination);
+		});
+		if ($opt{install_paths}->is_default_installable('bindoc')) {
+			my $manpage = catfile('blib', 'libdoc', man3_pagename($source));
+			$graph->add_file($manpage, dependencies => [ $source, 'Build' ], dependents => ['man'], action => sub { _manify($source, $manpage, 1, \%opt) });
+		}
+	}
+
+	my $tester = TAP::Harness->new({verbosity => $opt{verbose}, lib => rel2abs(catdir(qw/blib lib/)), color => -t STDOUT});
+	$graph->add_phony('test', dependencies => ['build'], action => sub { $tester->runtests(sort +find(file => name => '*.t', in => 't'))->has_errors and die "Errors in testing.  Cannot continue.\n" });
+	$graph->add_phony('install', dependencies => ['build'], action => sub { install($opt{install_paths}->install_map, @opt{qw/verbose dry_run uninst/}); });
+
+	return $graph;
+}
 
 sub Build {
 	my $bpl = decode_json(read_file('_build_params'));
 	my $action = @ARGV && $ARGV[0] =~ /\A\w+\z/ ? shift @ARGV : 'build';
-	die "No such action '$action'\n" if not $actions{$action};
 	my $rc_opts = read_config();
 	my @env = defined $ENV{PERL_MB_OPT} ? split_like_shell($ENV{PERL_MB_OPT}) : ();
 	unshift @ARGV, map { @{$_} } grep { defined } $rc_opts->{'*'}, $bpl, $rc_opts->{$action}, \@env;
 	GetOptions(\my %opt, qw/install_base=s install_path=s% installdirs=s destdir=s prefix=s config=s% uninst:1 verbose:1 dry_run:1/);
 	$_ = detildefy($_) for grep { defined } @opt{qw/install_base destdir prefix/}, values %{ $opt{install_path} };
-	@opt{'config', 'meta'} = (ExtUtils::Config->new($opt{config}), _get_meta());
-	$actions{$action}->(%opt, install_paths => ExtUtils::InstallPaths->new(%opt, dist_name => $opt{meta}->name));
+	$opt{config} = ExtUtils::Config->new($opt{config});
+	$opt{meta} = _get_meta();
+	$opt{install_paths} = ExtUtils::InstallPaths->new(%opt, dist_name => $opt{meta}->name);
+	_get_graph(%opt)->run($action);
 }
 
 sub Build_PL {
@@ -102,7 +116,7 @@ Traditionally, Build.PL uses Module::Build as the underlying build system.
 This module provides a simple, lightweight, drop-in replacement.
 
 Whereas Module::Build has over 6,700 lines of code; this module has less
-than 70, yet supports the features needed by most pure-Perl distributions.
+than 90, yet supports the features needed by most pure-Perl distributions.
 
 =head2 Supported
 
