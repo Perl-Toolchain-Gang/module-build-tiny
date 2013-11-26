c4cfeca37dde0bf0c01f5f01ae25a48dd8e38d8d
Leon Timmermans <fawaka@gmail.com>
2013-11-27 00:35:50 +0100

    Use mod2fname appropriately
    This will ensure loadable objects have an appropriate name on platforms
    with particular demands, such as VMS, OS2 and Android.

--- lib/Module/Build/Tiny.pm
+++ lib/Module/Build/Tiny.pm
@@ -47,9 +47,9 @@ sub process_xs {
 	my ($source, $options) = @_;
 
 	die "Can't build xs files under --pureperl-only\n" if $options->{'pureperl-only'};
-	my (undef, @dirnames) = splitdir(dirname($source));
-	my $file_base = basename($source, '.xs');
-	my $archdir = catdir(qw/blib arch auto/, @dirnames, $file_base);
+	my (undef, @parts) = splitdir(dirname($source));
+	push @parts, my $file_base = basename($source, '.xs');
+	my $archdir = catdir(qw/blib arch auto/, @parts);
 	my $tempdir = 'temp';
 
 	my $c_file = catfile($tempdir, "$file_base.c");
@@ -62,9 +62,12 @@ sub process_xs {
 	my $builder = ExtUtils::CBuilder->new(config => $options->{config}->values_set);
 	my $ob_file = $builder->compile(source => $c_file, defines => { VERSION => qq/"$version"/, XS_VERSION => qq/"$version"/ }, include_dirs => [ curdir ]);
 
+	require DynaLoader;
+	my $mod2fname = defined &DynaLoader::mod2fname ? \&DynaLoader::mod2fname : sub { return $_[0][-1] };
+
 	mkpath($archdir, $options->{verbose}, oct '755') unless -d $archdir;
-	my $lib_file = catfile($archdir, "$file_base." . $options->{config}->get('dlext'));
-	return $builder->link(objects => $ob_file, lib_file => $lib_file, module_name => join '::', @dirnames, $file_base);
+	my $lib_file = catfile($archdir, $mod2fname->(\@parts) . '.' . $options->{config}->get('dlext'));
+	return $builder->link(objects => $ob_file, lib_file => $lib_file, module_name => join '::', @parts);
 }
 
 sub find {
