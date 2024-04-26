sysfce2/perl-module-build-tiny

Implement dynamic prerequisites

lib/Module/Build/Tiny.pm
@@ -188,6 +188,15 @@ sub Build_PL {
 	make_executable('Build');
 	my @env = defined $ENV{PERL_MB_OPT} ? split_like_shell($ENV{PERL_MB_OPT}) : ();
 	write_file('_build_params', encode_json([ \@env, \@ARGV ]));
+	if (my $dynamic = $meta->custom('x_dynamic_prereqs')) {
+		my %meta = (%{ $meta->as_struct }, dynamic_config => 0);
+		my %opt = get_arguments(\@env, \@ARGV);
+		require CPAN::Requirements::Dynamic;
+		my $dynamic_parser = CPAN::Requirements::Dynamic->new(%opt);
+		my $prereq = $dynamic_parser->parse($dynamic);
+		$meta{prereqs} = $meta->effective_prereqs->with_merged_prereqs($prereq)->as_string_hash;
+		$meta = CPAN::Meta->new(\%meta);
+	}
 	$meta->save(@$_) for ['MYMETA.json'], [ 'MYMETA.yml' => { version => 1.4 } ];
 }
 
@@ -228,13 +237,13 @@ than 200, yet supports the features needed by most distributions.
 
 =item * Module sharedirs
 
+=item * Dynamic prerequisites
+
 =back
 
 =head2 Not Supported
 
 =over 4
-
-=item * Dynamic prerequisites
 
 =item * HTML documentation generation
 

Perl-Toolchain-Gang/module-build-tiny

lib/Module/Build/Tiny.pm
@@ -188,6 +188,15 @@ sub Build_PL {
 	make_executable('Build');
 	my @env = defined $ENV{PERL_MB_OPT} ? split_like_shell($ENV{PERL_MB_OPT}) : ();
 	write_file('_build_params', encode_json([ \@env, \@ARGV ]));
+	if (my $dynamic = $meta->custom('x_dynamic_prereqs')) {
+		my %meta = (%{ $meta->as_struct }, dynamic_config => 0);
+		my %opt = get_arguments(\@env, \@ARGV);
+		require CPAN::Requirements::Dynamic;
+		my $dynamic_parser = CPAN::Requirements::Dynamic->new(%opt);
+		my $prereq = $dynamic_parser->evaluate($dynamic);
+		$meta{prereqs} = $meta->effective_prereqs->with_merged_prereqs($prereq)->as_string_hash;
+		$meta = CPAN::Meta->new(\%meta);
+	}
 	$meta->save(@$_) for ['MYMETA.json'], [ 'MYMETA.yml' => { version => 1.4 } ];
 }
 
@@ -228,13 +237,13 @@ than 200, yet supports the features needed by most distributions.
 
 =item * Module sharedirs
 
+=item * Dynamic prerequisites
+
 =back
 
 =head2 Not Supported
 
 =over 4
-
-=item * Dynamic prerequisites
 
 =item * HTML documentation generation
 